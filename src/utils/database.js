77f123a7a19799c3a13932acec74a26aa4d85bc4
Julio Cesar <billyjoplin112@gmail.com>
2023-10-10 01:00:06 -0600

    commit1

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,6 +3,12 @@ const config = require('../config')
 
 const db = new Sequelize({
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // Requiere SSL
+      rejectUnauthorized: false, // Desactiva la verificación del certificado (no recomendado en producción)
+    },
+  },
   host: config.db.host,
   username: config.db.useName,
   password: config.db.password,
